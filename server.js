99a52716a447d4d813c6b1889d60cfcef1f011e6
Reade <readelobdill@aim.com>
2016-05-27 16:20:04 -0700

    Revert "backtrack a bit"
    This reverts commit 63e6d3cb5ed7d8048bf9ebfb35cd26fb93fef298.

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
+// var sql = require('mssql'),
+// 	sqlConfig = {
+// 	    user: 'coachseek@idlf08a7kb',
+// 	    password: 'W#ggie1267',
+// 	    server: 'idlf08a7kb.database.windows.net',
+// 	    database: 'Coachseek-Api-Testing',
+
+// 	    options: {
+// 	        encrypt: true
+// 	    }
+// 	}
+
+// sql.connect(sqlConfig).then(function() {
+// 	console.log("CONNECTED");
+// });
+
+// sql.on('error', function(err) {
+// 	console.log(err);
+// });
+
 var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
+app.get('/', function(req,res) {
+    res.send('hello');
+});
 
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+app.get('/update-sub', function (req, res) {
+  res.send('UPDATE SUB')
+});
 
-}).listen(process.env.PORT || 8080);
+http.createServer(app).listen(8080);
