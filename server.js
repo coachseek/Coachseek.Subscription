63e6d3cb5ed7d8048bf9ebfb35cd26fb93fef298
Reade <readelobdill@aim.com>
2016-05-27 16:15:37 -0700

    backtrack a bit

--- server.js
+++ server.js
@@ -1,33 +1,8 @@
-// var sql = require('mssql'),
-// 	sqlConfig = {
-// 	    user: 'coachseek@idlf08a7kb',
-// 	    password: 'W#ggie1267',
-// 	    server: 'idlf08a7kb.database.windows.net',
-// 	    database: 'Coachseek-Api-Testing',
-
-// 	    options: {
-// 	        encrypt: true
-// 	    }
-// 	}
-
-// sql.connect(sqlConfig).then(function() {
-// 	console.log("CONNECTED");
-// });
-
-// sql.on('error', function(err) {
-// 	console.log(err);
-// });
-
 var http = require('http');
-var express = require('express');
-var app = express();
 
-app.get('/', function(req,res) {
-    res.send('hello');
-});
+http.createServer(function (req, res) {
 
-app.get('/update-sub', function (req, res) {
-  res.send('UPDATE SUB')
-});
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world!');
 
-http.createServer(app).listen(8080);
+}).listen(process.env.PORT || 8080);
